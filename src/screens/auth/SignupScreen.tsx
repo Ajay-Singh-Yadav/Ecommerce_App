7eb5a9abdd9b9bf8b72b41d00a219690e7ea5fed
Ajay Yadav <ajay.yadav@iamtechie.com>
2026-01-29 17:24:04 +0530

    add screens

---
+++ src/screens/auth/SignupScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignupScreen = () => {
+  return (
+    <View  style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>SignupScreen</Text>
+    </View>
+  )
+}
+
+export default SignupScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/wishlist/WishlistScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WishlistScreen = () => {
+  return (
+     <View  style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>WishlistScreen</Text>
+    </View>
+  )
+}
+
+export default WishlistScreen
+
+const styles = StyleSheet.create({})
